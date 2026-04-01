946a7f231786ecb7a8196237c85219574cd343cf
ganotrasanyam-blip <ganotrasanyam@gmail.com>
2026-04-02 00:47:08 +0530

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,20 +9,20 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              AKASH
+              SANYAM
               <br />
-              <span>MALHOTRA</span>
+              <span>GANOTRA</span>
             </h1>
           </div>
           <div className="landing-info">
-            <h3>Co-Founder &</h3>
+            <h3>Social Media Strategist</h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Tech</div>
-              <div className="landing-h2-2">Business</div>
+              <div className="landing-h2-1">Virality</div>
+              <div className="landing-h2-2">Curiosity</div>
             </h2>
             <h2>
-              <div className="landing-h2-info">Business</div>
-              <div className="landing-h2-info-1">Tech</div>
+              <div className="landing-h2-info">Content</div>
+              <div className="landing-h2-info-1">Creativity</div>
             </h2>
           </div>
         </div>
